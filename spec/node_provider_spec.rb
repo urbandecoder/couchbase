2550a94f9040fe14240a27d28952817920f56e7e
Sebastian Brandt <sebbrandt87+github@gmail.com>
2014-11-16 09:17:35 +0100

    restores old spec password

--- spec/node_provider_spec.rb
+++ spec/node_provider_spec.rb
@@ -14,7 +14,7 @@ describe Chef::Provider::CouchbaseNode do
       :name => "my node",
       :id => id,
       :username => "Administrator",
-      :password => "23[]2321/dsskksldö,",
+      :password => "password",
       :database_path => new_database_path,
       :index_path => new_index_path,
       :updated_by_last_action => nil,
