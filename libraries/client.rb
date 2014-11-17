da47d3fa83e8ce09a6889d8416e9f944622d53d0
Sebastian Brandt <sbrandt@goodgamestudios.com>
2014-11-17 14:37:29 +0100

    updated to working state with real http basic auth in http header

--- libraries/client.rb
+++ libraries/client.rb
@@ -3,11 +3,17 @@ module Couchbase
     private
 
     def uri_from_path(path)
-      URI.parse "http://" + URI.encode(@new_resource.username) + ":" + URI.encode(@new_resource.password) + "@localhost:8091" + URI.encode(path)
+      escaped_uri = URI.escape "http://localhost:8091#{path}"
+      URI.parse escaped_uri
     end
 
     def post(path, params)
-      response = Net::HTTP.post_form(uri_from_path(path), params)
+      uri = uri_from_path path
+
+      req = Net::HTTP::Post.new(uri.path)
+      req.basic_auth @new_resource.username, @new_resource.password
+      req.set_form_data(params)
+      response = Net::HTTP.new(uri.host, uri.port).start {|http| http.request(req) }
       Chef::Log.error response.body unless response.kind_of? Net::HTTPSuccess
       response.value
       response
@@ -18,7 +24,7 @@ module Couchbase
 
       response = Net::HTTP.start uri.host, uri.port do |http|
         request = Net::HTTP::Get.new uri.path
-        request.basic_auth uri.user, uri.password
+        request.basic_auth @new_resource.username, @new_resource.password
         http.request request
       end
     end
