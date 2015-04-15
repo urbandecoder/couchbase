23854f22dade846119e5dffc9975794420aa0038
mwrock <matt@mattwrock.com>
2015-04-14 22:51:17 -0700

    serverspec tests now work on windows

--- test/integration/server-community/serverspec/service_spec.rb
+++ test/integration/server-community/serverspec/service_spec.rb
@@ -2,28 +2,42 @@ require 'serverspec'
 require "json"
 require "net/http"
 
-set :backend, :exec
+require 'rbconfig'
+case RbConfig::CONFIG['host_os']
+when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+  set :backend, :cmd
+  set :os, :family => 'windows'
+else
+  set :backend, :exec
+end
 
-describe service('couchbase-server') do
+node = JSON.parse(IO.read(File.join(ENV["TEMP"] || "/tmp", "export-node/node.json")))["default"]["couchbase"]["server"]
+
+describe service(node["service_name"]) do
   it { should be_enabled }
   it { should be_running }
 end
 
-describe file("/opt/couchbase/var/lib/couchbase/logs") do
+describe file(node["log_dir"]) do
   it { should be_directory }
-  it { should be_owned_by "couchbase" }
-  it { should be_grouped_into "couchbase" }
+  unless host_inventory['platform'] == "windows"
+    it { should be_owned_by "couchbase" }
+    it { should be_grouped_into "couchbase" }
+  end
 end
 
-describe file("/opt/couchbase/etc/couchbase/static_config") do
-  its(:content) { should contain "{error_logger_mf_dir, \"/opt/couchbase/var/lib/couchbase/logs\"}." }
+describe file(File.join(node["install_dir"], "etc/couchbase/static_config")) do
+  let(:node) { JSON.parse(IO.read(File.join(ENV["TEMP"] || "/tmp", "/tmp/export-node/node.json")))["default"]["couchbase"]["server"] }
+  its(:content) { should contain "{error_logger_mf_dir, \"#{node["log_dir"]}\"}." }
   its(:content) { should_not match /error_logger_mf_dir.*error_logger_mf_dir/ }
 end
 
-describe file("/opt/couchbase/var/lib/couchbase/data") do
+describe file(node["database_path"]) do
   it { should be_directory }
-  it { should be_owned_by "couchbase" }
-  it { should be_grouped_into "couchbase" }
+  unless host_inventory['platform'] == "windows"
+    it { should be_owned_by "couchbase" }
+    it { should be_grouped_into "couchbase" }
+  end
 end
 
 describe "couchbase service state" do
@@ -37,24 +51,22 @@ describe "couchbase service state" do
   end
 
   describe "self node" do
-    let(:data_dir) { "/opt/couchbase/var/lib/couchbase/data" }
     let(:path) { "/nodes/self" }
 
     it "has its database path configured" do
-      expect(response["storage"]["hdd"][0]["path"]).to eq data_dir
+      expect(response["storage"]["hdd"][0]["path"]).to eq node["database_path"]
     end
 
     it "has its index path configured" do
-      expect(response["storage"]["hdd"][0]["index_path"]).to eq data_dir
+      expect(response["storage"]["hdd"][0]["index_path"]).to eq node["database_path"]
     end
   end
 
   describe "default cluster" do
     let(:path) { "/pools/default" }
-    let(:node) {JSON.parse(IO.read('/tmp/kitchen/chef_node.json'))}
 
     it "has its memory quota configured" do
-      expect((response["storageTotals"]["ram"]["quotaTotal"] / response["nodes"].size / 1024 / 1024).to_i).to eq node["default"]["couchbase"]["server"]["memory_quota_mb"]
+      expect((response["storageTotals"]["ram"]["quotaTotal"] / response["nodes"].size / 1024 / 1024).to_i).to eq node["memory_quota_mb"]
     end
   end  
 end
