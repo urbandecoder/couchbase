910f171135f89d46fe19bd866604f5fb92857f90
mwrock <matt@mattwrock.com>
2015-04-16 09:31:41 -0700

    tests scope to the platform when searching for primary node and stash the primary ip in a node attribute so it can be verified in a test

--- test/integration/cookbooks/couchbase-tests/recipes/default.rb
+++ test/integration/cookbooks/couchbase-tests/recipes/default.rb
@@ -1,12 +1,11 @@
 include_recipe "couchbase::server"
 
-primary = search_for_nodes("run_list:*couchbase??server*")
-primary_ip = primary[0]['ipaddress']
-
+primary = search_for_nodes("run_list:*couchbase??server* AND platform:#{node['platform']}")
+node.normal["couchbase-tests"]["primary_ip"] = primary[0]['ipaddress']
 ruby_block "block_until_operational" do
   block do
     Chef::Log.info "Waiting until the Couchbase admin API is responding"
-    test_url = URI.parse("http://#{primary_ip}:#{node['couchbase']['server']['port']}")
+    test_url = URI.parse("http://#{node["couchbase-tests"]["primary_ip"]}:#{node['couchbase']['server']['port']}")
     until CouchbaseHelper.endpoint_responding?(test_url) do
       sleep 1
       Chef::Log.debug(".")
@@ -17,7 +16,7 @@ end
 couchbase_cluster "default" do
   action :join
   memory_quota_mb node['couchbase']['server']['memory_quota_mb']
-  hostname primary_ip
+  hostname node["couchbase-tests"]["primary_ip"]
   username node['couchbase']['server']['username']
   password node['couchbase']['server']['password']
 end
