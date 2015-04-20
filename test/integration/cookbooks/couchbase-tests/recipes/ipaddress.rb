7921009b1f1e67e2fe8fa22a09a473975cb4b593
mwrock <matt@mattwrock.com>
2015-04-19 23:41:07 -0700

    ipaddress recipe now works for linux/vbox

--- test/integration/cookbooks/couchbase-tests/recipes/ipaddress.rb
+++ test/integration/cookbooks/couchbase-tests/recipes/ipaddress.rb
@@ -1,5 +1,16 @@
-if node["kernel"]["cs_info"]["model"] == "VirtualBox"
+# linux
+if node["virtualization"] && node["virtualization"]["system"] == "vbox"
+  interface = node["network"]["interfaces"].select {|key| puts key;key == "eth1"}
+  unless interface.empty?
+    interface["eth1"]["addresses"].each do |ip, params|
+      if params['family'] == ('inet')
+        node.automatic["ipaddress"] = ip
+      end
+    end
+  end
+# windows
+elsif node["kernel"] && node["kernel"]["cs_info"] && node["kernel"]["cs_info"]["model"] == "VirtualBox"
   interfaces = node["network"]["interfaces"]
-  ik = interfaces.keys
-  node.automatic["ipaddress"] = interfaces[ik.last]["configuration"]["ip_address"][0]
+  interface_key = interfaces.keys.last
+  node.automatic["ipaddress"] = interfaces[interface_key]["configuration"]["ip_address"][0]
 end
